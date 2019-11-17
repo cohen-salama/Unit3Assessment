5e094ffa08cdd7d95934cce4e13c432d28e8c63e
sergiocohens <sergiocohensalama@pursuit.org>
2019-11-17 11:44:41 -0500

    researchers router (untested)

---
+++ routes/researchersRouter.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const router = express.Router()
+
+const Researchers = require('../models/Researchers')
+
+router.get('/', async (req, res) => {
+  try {
+    const researchers = await Researchers.getAll()
+    res.json({
+      status: 'success',
+      message: 'retrieved all researchers',
+      payload: researchers
+  })
+  } catch {
+    res.json({
+      status: 'error',
+      message: 'no researchers found',
+      payload: null
+  })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const researcher = await Researchers.getById(id)
+    res.json({
+      status: 'success',
+      message: 'retrieved single researcher',
+      payload: researcher
+    })
+  } catch {
+    res.json({
+      status: 'error',
+      message: 'researcher not found',
+      payload: null
+    })
+  }
+})
+
+router.post('/', async (req, res) => {
+  const body = {
+    ...req.body
+  }
+  try {
+    const researcher = await Researchers.addNew(body)
+    res.json({
+      status: 'success',
+      message: 'researcher added',
+      payload: researcher
+    })
+  } catch {
+    res.json({
+      status: 'error',
+      message: 'researcher not added',
+      payload: null
+    })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const body = {
+    ...req.body
+  }
+  try {
+    const updated = await Researchers.updateById(body)
+    res.json({
+      status: 'success',
+      message: 'researcher updated',
+      payload: updated
+    })
+  } catch {
+    res.json({
+      status: 'error',
+      message: 'researcher not updated',
+      payload: null
+    })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const deleted await Researchers.deleteById(id)
+    res.json({
+      status: 'success',
+      message: 'researcher deleted',
+      payload: deleted
+    })
+  } catch {
+    res.json({
+      status: 'error',
+      message: 'researcher not deleted',
+      payload: null
+    })
+  }
+})
+})
