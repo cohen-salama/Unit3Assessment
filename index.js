96045c8eca7244e372f8488bb707b8950a2568f7
sergiocohens <sergiocohensalama@pursuit.org>
2019-11-17 16:38:49 -0500

    no input reminder, final fixes

--- index.js
+++ index.js
@@ -14,30 +14,42 @@ document.addEventListener('DOMContentLoaded', () => {
 const allSightings = async () => {
   let container = document.getElementById('container')
   container.innerText = ''
+
   let response = await axios.get('http://localhost:3030/sightings')
   let dataArray = response.data.payload
-  for (el of dataArray) {
-    let researcher = await axios.get(`http://localhost:3030/researchers/${el.researcher_id}`)
-    let species = await axios.get(`http://localhost:3030/species/${el.species_id}`)
-    let habitat = await axios.get(`http://localhost:3030/habitats/${el.habitat_id}`)
-    let sighting = document.createElement('p')
-    sighting.innerText = `An ${species.data.payload.name} was spotted by ${researcher.data.payload.name} in the ${habitat.data.payload.category}`
-    container.appendChild(sighting)
+  if (dataArray.length >= 1) {
+    for (el of dataArray) {
+      let researcher = await axios.get(`http://localhost:3030/researchers/${el.researcher_id}`)
+      let species = await axios.get(`http://localhost:3030/species/${el.species_id}`)
+      let habitat = await axios.get(`http://localhost:3030/habitats/${el.habitat_id}`)
+
+      let sighting = document.createElement('p')
+      sighting.innerText = `An ${species.data.payload.name} was spotted by ${researcher.data.payload.name} in the ${habitat.data.payload.category}`
+      container.appendChild(sighting)
+    }
+  } else {
+    container.innerText = 'No sightings.'
   }
 }
 
 const sightingsByResearcher = async () => {
   let container = document.getElementById('container')
   container.innerText = ''
+
   let id = document.getElementById('researcherInput').value
   let response = await axios.get(`http://localhost:3030/sightings/researchers/${id}`)
   let dataArray = response.data.payload
-  for (el of dataArray) {
-    let researcher = await axios.get(`http://localhost:3030/researchers/${el.researcher_id}`)
-    let species = await axios.get(`http://localhost:3030/species/${el.species_id}`)
-    let habitat = await axios.get(`http://localhost:3030/habitats/${el.habitat_id}`)
-    let sighting = document.createElement('p')
-    sighting.innerText = `An ${species.data.payload.name} was spotted by ${researcher.data.payload.name} in the ${habitat.data.payload.category}`
-    container.appendChild(sighting)
+  if (dataArray !== null) {
+    for (el of dataArray) {
+      let researcher = await axios.get(`http://localhost:3030/researchers/${el.researcher_id}`)
+      let species = await axios.get(`http://localhost:3030/species/${el.species_id}`)
+      let habitat = await axios.get(`http://localhost:3030/habitats/${el.habitat_id}`)
+
+      let sighting = document.createElement('p')
+      sighting.innerText = `An ${species.data.payload.name} was spotted by ${researcher.data.payload.name} in the ${habitat.data.payload.category}`
+      container.appendChild(sighting)
+    }
+  } else {
+    container.innerText = '---------------------------------------------------------- Enter Researcher ID'
   }
 }
